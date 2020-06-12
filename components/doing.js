e3b6295655b671894a918c6b9bd3fa3ffad85c2f
mchaudhry05 <39622610+mchaudhry05@users.noreply.github.com>
2020-06-11 23:03:05 -0400

    initial commit

---
+++ components/doing.js
@@ -0,0 +1,26 @@
+import  React, {Component} from 'react'; 
+
+class Doing extends Component{
+    render(){
+        return(
+            <div className="doing">
+                <h1 className="type">Doing Here</h1>
+                <div className="container-doing">
+                <div className="task-container-doing">
+                    {
+                        this.props.item.filter(todo => todo.isDoing).map(filteredTodo => (
+                        <div className="task">
+                            <button onClick={e => this.props.left(filteredTodo.content, e.id)} className="button-left"id={filteredTodo.id}></button>
+                            <h4 className="task-list">{filteredTodo.content}</h4>
+                            <button onClick={e => this.props.right(filteredTodo.content, e.id)}className="button-right"id={filteredTodo.id}></button>
+                        </div>
+                        ))
+                    } 
+                </div>
+                </div>
+            </div>
+        ); 
+    }
+}
+
+export default Doing;
---
+++ components/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'; 
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="header-container">
+                <h1 className="header">Workflow</h1>
+            </div>
+            
+            
+
+        );
+    }
+}
+
+export default Header; 
